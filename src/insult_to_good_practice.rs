ff9946a6d79b0e3406956473da9ddca8b3b895c7
Augustin LOPEZ <augustin@augustins-macbook-air.home>
2021-02-18 12:02:41 +0100

    replaced newton by babylon

--- src/insult_to_good_practice.rs
+++ src/insult_to_good_practice.rs
@@ -3,14 +3,14 @@ pub fn sqrt_for_moron(getlost: f64) -> f64 {
 		eprintln!("I don't care");
 		return 0.0;
 	}
-	let mut newtonerie = match getlost {
+	let mut babylonianerie = match getlost {
 		d if d > 1.0 => getlost,
 		_ => 1.0,
 	};
 	loop {
-		let previous = newtonerie;
-		newtonerie = (getlost / newtonerie + newtonerie) / 2.0;
-		if newtonerie >= previous {
+		let previous = babylonianerie;
+		babylonianerie = (getlost / babylonianerie + babylonianerie) / 2.0;
+		if babylonianerie >= previous {
 			return previous;
 		}
 	}
